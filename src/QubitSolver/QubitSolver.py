0280e1f1b3c176c27277f45845bf81efe1c2f41a
9674036-code <9674036@graniteschools.org>
2026-04-21 19:27:28 -0600

    Refactor QubitSolver to load circuit from file

--- src/QubitSolver/QubitSolver.py
+++ src/QubitSolver/QubitSolver.py
@@ -2,51 +2,109 @@ import pygame
 from Qubit import Qubit
 from Button import Button
 from QuantumCircut import QuantumCircut
-qCircut = QuantumCircut(2)
+
+
+with open("save.txt","r") as save: 
+ qCircut = QuantumCircut(int(save.read().strip()))
 # (val, x, y, w, l, c1, c2)
-buttons = [Button("X", 100, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
-           Button("H", 200, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
-           Button("CNOT", 300, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
-           Button("Measure", 10, 10, 130, 80, (200, 200, 200), (20, 40, 20))]
+buttons = [Button("How To Use", 420, 360, 130, 80, (200, 200, 200), (20, 40, 20)),
+          Button("Start", 420, 270, 130, 80, (200, 200, 200), (20, 40, 20))]
+
 
 def modQubits():
- pass
+pass
+
 
 def update():
- pass
+pass
 
-def reDis():
- pass
+
+def reDis(isComp=False):
+screen.fill((0, 0, 0))
+for i in buttons:
+ i.display(screen, font, False)
+qCircut.display(screen,font)
+if isComp:
+   results=[]
+   compBasis=""
+
 
 pygame.init()
 results=[]
+mode="s"
 compBasis=""
-font = pygame.font.Font("Roboto-Black.ttf", 30)
+font = pygame.font.Font("Roboto-Black.ttf", 20)
+sFont = pygame.font.Font("Roboto-Black.ttf", 50)
+
+
 screen = pygame.display.set_mode((960, 720))
 clock = pygame.time.Clock()
 running = True
 compText = font.render("", True, (255, 255, 255))
 screen.fill((0, 0, 0))
 for i in buttons:
-  i.display(screen, font, False)
+ i.display(screen, font, False)
+
+
+sText=sFont.render("Qubit Solver", True, (255, 255, 255))
+screen.blit(sText, (350, 50))
+
+
 while running:
- for event in pygame.event.get():
+if mode=="s":
+   for event in pygame.event.get():
      if event.type == pygame.QUIT:
          running = False
      if event.type == pygame.MOUSEBUTTONDOWN:
-        for i in buttons:
+         for i in buttons:
+           i.hover()
+           i.display(screen, font, True)
+           if i.over == True:
+               if i.val == "How To Use":
+                   reDis(True)
+               elif i.val == "Start":
+                  buttons=[]
+                  buttons = [Button("X", 100, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
+                             Button("H", 200, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
+                             Button("CNOT", 300, 630, 90, 80, (200, 200, 200), (20, 40, 20)),
+                             Button("Measure", 10, 10, 130, 80, (200, 200, 200), (20, 40, 20)),
+                             Button("How To Use", 10, 100, 130, 80, (200, 200, 200), (20, 40, 20)),
+                             Button("New", 10, 190, 130, 80, (200, 200, 200), (20, 40, 20))]
+                  mode="p"
+                  reDis()
+   for i in buttons:
+     i.hover()
+     i.display(screen, font, False)
+elif mode=="p":
+ for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+         with open ("save.txt","w") as save:
+           save.write(qCircut.level)
+         running = False
+     if event.type == pygame.MOUSEBUTTONDOWN:
+         for i in buttons:
            i.hover()
            i.display(screen, font, True)
            if i.over == True:
                if i.val == "Measure":
-                  results.append(qCircut.compBasis())
-                  compBasis = f"Measurements: {', '.join(results)}"
-                  compText = font.render(compBasis, True, (255, 255, 255))
+                   results.append(qCircut.compBasis())
+                   compBasis = f"Measurements: {', '.join(results)}"
+                   compText = font.render(compBasis, True, (255, 255, 255))
+                   screen.blit(compText, (20, 570))
+               elif i.val == "X":
+                   reDis(True)
+               elif i.val == "CNOT":
+                   reDis(True)
+               elif i.val == "H":
+                   reDis(True)
+               elif i.val == "New":
+                   qCircut=QuantumCircut(1)
+                   reDis(True)
+
+
  for i in buttons:
      i.hover()
      i.display(screen, font, False)
- qCircut.display(screen,font)
- screen.blit(compText, (20, 570))
- pygame.display.flip()
- clock.tick(60)
+pygame.display.flip()
+clock.tick(60)
 pygame.quit()
