4f4ffe7ab1e692b84b5fffd17a03e33022938206
9674036-code <9674036@graniteschools.org>
2026-04-28 12:43:01 -0600

    Add files via upload

---
+++ src/temp/QuantumGate.py
@@ -0,0 +1,34 @@
+import numpy as np
+from functools import reduce
+
+class QuantumGate:
+  gates = {'X': np.array([
+      [0, 1],
+      [1, 0]]),
+      'H': np.array([
+      [1, 1],
+      [1, -1]])
+      *(1/np.sqrt(2)),
+      'CNOT': np.array([
+      [1, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0]
+  ])
+  }
+  I=np.array([[1,0],[0,1]])
+  def __init__(self, type, qubits):
+      self.type = type
+      self.qubits = qubits
+
+
+  def compute(self, qubit, nQubits):
+    gates=[]
+    for i in range(nQubits):
+        if i==self.qubits:
+            gates.append(QuantumGate.gates[self.type])
+        else:
+            gates.append(QuantumGate.I)
+    gate=reduce(np.kron,gates)
+    print(qubit @ gate)
+    return qubit @ gate
