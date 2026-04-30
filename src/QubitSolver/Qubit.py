733f23c26ae6b9c3ccbeef3c040263a28122ba74
9674036-code <9674036@graniteschools.org>
2026-04-30 12:40:28 -0600

    Update Qubit.py

--- src/QubitSolver/Qubit.py
+++ src/QubitSolver/Qubit.py
@@ -2,6 +2,7 @@ import numpy as np
 class Qubit:
  def __init__(self, state):
      self.state = state
+     self.gates=[]
 
  def compBasis(self):
      return f"0: {100*self.state[0]}% 1: {100*self.state[0]}%"
