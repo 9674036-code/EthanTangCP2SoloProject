2a076365b5d36cbce795b1854620bf1f3de357d8
9674036-code <9674036@graniteschools.org>
2026-04-30 12:39:29 -0600

    Update QubitSolver.py

--- src/QubitSolver/QubitSolver.py
+++ src/QubitSolver/QubitSolver.py
@@ -3,7 +3,7 @@ from Qubit import Qubit
 from Button import Button
 from QuantumCircut import QuantumCircut
 from QuantumGate import QuantumGate
-
+import numpy as np
 
 with open("save.txt", "r") as save:
     qCircut = QuantumCircut(int(save.read().strip()))
@@ -15,7 +15,8 @@ buttons = [Button("How To Use", 30, 360, 130, 130, (10, 10, 10), (18, 3, 37)),
 pygame.init()
 results = []
 hitB = []
-gates = []
+gates = {}
+cGate={"|0>":np.array([1, 0]),"|1>":np.array([0, 1])}
 current = ""
 close = 1
 mode = "s"
@@ -45,8 +46,11 @@ def reDis(isComp=False):
    if isComp:
        results = []
        compBasis = ""
+   print(gates)
+   current=""
    for i in gates:
-       pygame.draw.rect(screen, (230,230,230),(i[0],i[1],30,30),0,10)
+       for i2 in gates[i]:
+        pygame.draw.rect(screen, (230,230,230),(i2[0],i2[1],30,30),0,10)
 
 def selectCheck(val, index):
    global current
@@ -79,11 +83,11 @@ while running:
                            reDis()
                        elif i.val == "Start":
                            buttons = []
-                           buttons = [ Button("X", 100, 630, 90, 80,
+                           buttons = [ Button("X", 140, 630, 90, 80,
                                                (10, 10, 10), (18, 3, 37)),
-                                       Button("H", 200, 630, 90, 80,
+                                       Button("H", 240, 630, 90, 80,
                                                (10, 10, 10), (18, 3, 37)),
-                                       Button("CNOT", 300, 630, 90, 80,
+                                       Button("CNOT", 340, 630, 90, 80,
                                                (10, 10, 10), (18, 3, 37)),
                                        Button("Measure", 10, 10, 130, 80,
                                                (10, 10, 10), (18, 3, 37)),
@@ -91,9 +95,9 @@ while running:
                                                (10, 10, 10), (18, 3, 37)),
                                        Button("Delete", 10, 190, 130, 80,
                                              (10, 10, 10), (18, 3, 37)),
-                                       Button("|0>", 300, 790, 90, 80,
+                                       Button("|0>", 30, 290, 90, 80,
                                              (10, 10, 10), (18, 3, 37)),
-                                       Button("|1>", 300, 700, 90, 80,
+                                       Button("|1>", 30, 380, 90, 80,
                                              (10, 10, 10), (18, 3, 37)),]
                            mode = "p"
                            reDis()
@@ -123,24 +127,36 @@ while running:
                            qCircut = QuantumCircut(1)
                            reDis(True)
                            hitB = updateHit()
-                           print(hitB)
+                           gates=[]
                        elif buttons[i].val == "Delete":
                            try:
                                del gates[len(gates)-1]
                                reDis()
                            except:
                                print("No gates to delete")
+                       elif "|" in buttons[i].val:
+                        if np.array_equal(cGate[buttons[i].val],qCircut.qubits[0].state) and qCircut.level+1 in QuantumCircut.levels:
+                           qCircut = QuantumCircut(qCircut.level+1)
+                           reDis(True)
+                           hitB = updateHit()
+                           gates=[]
+                        else:
+                            mode="f"
                for i in range(len(hitB)):
-                   if hitB[i].over and current in QuantumGate.gates:
-                       qCircut.addGate(current, i)
-                       gates.append((((hitB[i].w/2)+hitB[i].x)-15,hitB[i].y+15))
-                       if i not in qCircut.gates:
-                           pygame.draw.rect(screen, (230, 230, 230),
-                            (((hitB[i].w/2)+hitB[i].x)-15, hitB[i].y+15, 30, 30), 0, 10)
-                       else:
+                    if hitB[i].over and current in QuantumGate.gates:
+                        if hitB[i].toggle==False:
+                            x,y=pygame.mouse.get_pos()
+                        try:
+                            if gates[i+1][len(gates)-1][0]<x+40:
+                                qCircut.addGate(current, i)
+                                gates.setdefault(i+1,[(x,hitB[i].y+15)])
+                                pygame.draw.rect(screen, (230, 230, 230),
+                                    (x, hitB[i].y+15, 30, 30), 0, 10)
+                        except:
+                            qCircut.addGate(current, i)
+                            gates.setdefault(i+1,[(x,hitB[i].y+15)])
                             pygame.draw.rect(screen, (230, 230, 230),
-                             (((hitB[i].w/(len(qCircut.gates[i])+1))+hitB[i].x)-15, hitB[i].y+15, 30, 30), 0, 10)
-
+                                    (x, hitB[i].y+15, 30, 30), 0, 10)
 
        for i in buttons:
            i.hover()
@@ -148,8 +164,9 @@ while running:
        for i in hitB:
            if i.hover():
                if current in QuantumGate.gates:
+                   x,y=pygame.mouse.get_pos()
                    pygame.draw.rect(screen, (230, 230, 230),
-                                   (((i.w/2)+i.x)-15, i.y+15, 30, 30), 0, 10)
+                                   (x, i.y+15, 30, 30), 0, 10)
                    print(True)
            elif i.hover()==False:
                reDis()
@@ -161,6 +178,14 @@ while running:
        eText = sFont.render(
            "Sucessfully saved, press the X again to quit the application.", True, (255, 255, 255))
        screen.blit(eText, (10, 300))
+   elif mode=="f":
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               running = False
+       screen.blit(backI,(0,0))
+       eText = sFont.render(
+           "You Have Finished All of the Levels", True, (255, 255, 255))
+       screen.blit(eText, (200, 300))
    pygame.display.flip()
    clock.tick(60)
 pygame.quit()
