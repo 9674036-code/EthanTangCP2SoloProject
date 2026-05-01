b1ea283bdb8770fbb8b648dfc1e3697ccc6f7fd2
9674036-code <9674036@graniteschools.org>
2026-04-30 20:59:36 -0600

    Refactor Button class for improved readability

--- src/QubitSolver/Button.py
+++ src/QubitSolver/Button.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 
+
 class Button:
    def __init__(self, val, x, y, w, l, c1, c2, toggle=False):
        self.val = val
@@ -11,41 +12,36 @@ class Button:
        self.c1 = c1
        self.c2 = c2
        self.over = False
-       self.toggle=toggle
-       self.nFirst=False
-
+       self.toggle = toggle
+       self.nFirst = False
 
    def display(self, screen, font, mouseClicked):
        if self.over == False:
            pygame.draw.rect(screen, self.c1, [
-                           self.x, self.y, self.w, self.l], 0, 30)
+               self.x, self.y, self.w, self.l], 0, 30)
        elif self.over == True and mouseClicked == False:
            pygame.draw.rect(screen, self.c2, [
-                           self.x, self.y, self.w, self.l], 0, 30)
-
+               self.x, self.y, self.w, self.l], 0, 30)
 
        text = font.render(self.val, True, (255, 255, 255))
        textRect = text.get_rect()
        textRect.center = (self.x + self.w/2, self.y + self.l/2)
        screen.blit(text, textRect)
 
-
    def hover(self):
        mouseX, mouseY = pygame.mouse.get_pos()
        if mouseX <= self.x + self.w and mouseX >= self.x and mouseY >= self.y and mouseY <= self.y + self.l:
            self.over = True
-           if self.toggle==False:
-               self.toggle=True
-               self.nFirst=False
-               print ('on')
+           if self.toggle == False:
+               self.toggle = True
+               self.nFirst = False
                return True
        else:
            self.over = False
-           if self.toggle and self.nFirst==False:
-               self.toggle=False
-               self.nFirst=True
-               print ('off')
+           if self.toggle and self.nFirst == False:
+               self.toggle = False
+               self.nFirst = True
                return False
            elif self.nFirst:
-               self.nFirst=False
+               self.nFirst = False
                return False
