6dcb7d1a159a6263378a80ee1752c53fceacd8c4
9674036-code <9674036@graniteschools.org>
2026-04-30 20:59:24 -0600

    Refactor QuantumCircuit class and update levels

--- src/QubitSolver/QuantumCircut.py
+++ src/QubitSolver/QuantumCircut.py
@@ -5,19 +5,21 @@ import pygame
 from functools import reduce
 pygame.init()
 
+
 class QuantumCircut:
-   levels = {1: [np.array([1, 0])], 2: [np.array([1,0]), np.array([0,1]),np.array([1,0])]}
+   levels = {1: [np.array([1, 0])], 2: [np.array(
+       [0, 1])],3:[np.array([1/np.sqrt(2), 1/np.sqrt(2)])],4:[np.array([1/np.sqrt(2), -1/np.sqrt(2)])]}
+
    def __init__(self, level):
        self.level = level
        self.qubits = []
-       self.gates={}
-       self.box=[]
+       self.gates = {}
+       self.box = []
        for i in QuantumCircut.levels[level]:
            self.qubits.append(Qubit(i))
        states = [i.state for i in self.qubits]
-       self.qSystem=reduce(np.kron,states)
-       self.nQSystem=self.qSystem
-       print(self.qSystem)
+       self.qSystem = reduce(np.kron, states)
+       self.nQSystem = self.qSystem
 
    def update(self, level):
        self.level = level
@@ -26,25 +28,29 @@ class QuantumCircut:
            self.qubits.append(Qubit(i))
 
    def display(self, surface, font):
-       self.nQSystem=self.qSystem
-       for i in self.gates:
-           self.nQSystem=self.gates[i].compute(self.nQSystem,len(self.qubits))
+       self.nQSystem = self.qSystem
        t = 550//(len(QuantumCircut.levels[self.level])+1)
-       self.box=[]
+       self.box = []
        for i in range(1, len(QuantumCircut.levels[self.level])+1):
-           pygame.draw.line(surface, (240, 240, 240), (200, i*t), (890, i*t), 5)
-           if i==1:
+           pygame.draw.line(surface, (240, 240, 240),
+                            (200, i*t), (890, i*t), 5)
+           if i == 1:
                text = font.render(f"|\u03C8>", True, (255, 255, 255))
-               surface.blit(text, (900, i*t-20))
+               #surface.blit(text, (900, i*t-20)) --- IGNORE --- Potential additional feature
            else:
-               text = font.render(f"|{(self.qubits[i-1].state[0])^1}>", True, (255, 255, 255))
+               text = font.render(
+                   f"|{(self.qubits[i-1].state[0]) ^ 1}>", True, (255, 255, 255))
            surface.blit(text, (150, i*t-20))
-           self.box.append((200,i*t))
+           self.box.append((200, i*t))
 
    def compBasis(self):
-       return self.qubits[0].compBasis()
- 
+       for i in self.gates:
+        for i2 in self.gates[i]:
+            self.nQSystem = i2.compute(
+               self.nQSystem, len(self.qubits))
+            print(self.nQSystem)
+       return self.qubits[0].compBasis(self.nQSystem)
+
    def addGate(self, gate, qNum):
-       self.gates.setdefault(qNum,QuantumGate(gate, qNum))
-       print(self.gates)
+       self.gates.setdefault(qNum, []).append(QuantumGate(gate, qNum))
        self.qubits[qNum].gates.append(gate)
