6d4fb24d92a1d7dc45871229234e370eeb2f0d60
9674036-code <9674036@graniteschools.org>
2026-05-01 10:48:00 -0600

    Delete src/temp directory

--- src/temp/Qubit.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-class Qubit:
-    def __init__(self, state):
-        self.state = state
-        self.gates = []
-    def compBasis(self):
-        return f"{np.random.choice([0, 1], p=[self.state[0], self.state[1]])}"
