38b3ece6d99c183645b3c125d02f6653a4465b3f
9674036-code <9674036@graniteschools.org>
2026-04-30 20:59:57 -0600

    Update Qubit class to enhance compBasis method
    Refactor compBasis method to use SVD and update state.

--- src/QubitSolver/Qubit.py
+++ src/QubitSolver/Qubit.py
@@ -1,8 +1,13 @@
 import numpy as np
+
 class Qubit:
  def __init__(self, state):
      self.state = state
-     self.gates=[]
+     self.nState=state
+     self.gates = []
 
- def compBasis(self):
-     return f"0: {100*self.state[0]}% 1: {100*self.state[0]}%"
+ def compBasis(self, system):
+    matrix=system.reshape(2,-1)
+    u,s,vh=np.linalg.svd(matrix)
+    self.nState=u[:,0]
+    return f"0: {round(100*np.square(np.abs(self.nState[0])))}% 1: {round(100*np.square(np.abs(self.nState[1])))}%"
